7caaef7a8fe107563046a34afd5a47292ef03fe6
dinesh-yadav <yadav.din@gmail.com>
2025-11-09 23:03:54 +0530

    Added rest apis to create, list, get quizzes into mysql db

---
+++ QuizService/src/main/java/com/quiz/QuizService/entities/Quiz.java
@@ -0,0 +1,22 @@
+package com.quiz.QuizService.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+}
