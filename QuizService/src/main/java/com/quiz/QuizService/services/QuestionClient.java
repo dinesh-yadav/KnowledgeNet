2fb37856bae004b486188eafe8b1708c7099a40a
dinesh-yadav <yadav.din@gmail.com>
2025-11-10 20:52:05 +0530

    QuizService/src/main/java/com/quiz/QuizService/QuizServiceApplication.java

--- QuizService/src/main/java/com/quiz/QuizService/services/QuestionClient.java
+++ QuizService/src/main/java/com/quiz/QuizService/services/QuestionClient.java
@@ -1,4 +1,15 @@
 package com.quiz.QuizService.services;
 
+import com.quiz.QuizService.entities.Question;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(url = "http://localhost:8082", value = "Question-Client")
 public interface QuestionClient {
+
+    @GetMapping("/questions/quizzes/{quizId}")
+    List<Question> getQuestionsOfQuiz(@PathVariable Long quizId);
 }
