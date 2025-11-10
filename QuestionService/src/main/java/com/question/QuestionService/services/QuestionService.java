40c7ca6d7b8324d968f504d3d06b798565e6897d
dinesh-yadav <yadav.din@gmail.com>
2025-11-10 19:13:24 +0530

    added QuestionService interface and it's implementation

--- QuestionService/src/main/java/com/question/QuestionService/services/QuestionService.java
+++ QuestionService/src/main/java/com/question/QuestionService/services/QuestionService.java
@@ -1,4 +1,15 @@
 package com.question.QuestionService.services;
 
+import com.question.QuestionService.entities.Question;
+
+import java.util.List;
+
 public interface QuestionService {
+    List<Question> get();
+
+    Question create(Question question);
+
+    Question get(Long questionId);
+
+    List<Question> getQuestionsOfQuiz(Long quizId);
 }
--- QuestionService/src/main/java/com/question/QuestionService/services/impl/QuestionServiceImpl.java
+++ QuestionService/src/main/java/com/question/QuestionService/services/impl/QuestionServiceImpl.java
@@ -1,4 +1,39 @@
 package com.question.QuestionService.services.impl;
 
-public class QuestionServiceImpl {
+import com.question.QuestionService.entities.Question;
+import com.question.QuestionService.repositories.QuestionRepository;
+import com.question.QuestionService.services.QuestionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionServiceImpl implements QuestionService {
+    private QuestionRepository questionRepository;
+
+    public QuestionServiceImpl(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    @Override
+    public List<Question> get() {
+        return questionRepository.findAll();
+    }
+
+    @Override
+    public Question create(Question question) {
+        return questionRepository.save(question);
+    }
+
+    @Override
+    public Question get(Long questionId) {
+        return questionRepository
+                .findById(questionId)
+                .orElseThrow(() -> new RuntimeException("question not found!!!"));
+    }
+
+    @Override
+    public List<Question> getQuestionsOfQuiz(Long quizId) {
+        return questionRepository.findByQuizId(quizId);
+    }
 }
